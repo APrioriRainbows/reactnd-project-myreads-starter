0ca1e62caa9476b0f5b9f2dcd8bdb6dd1784a281
Maria Caruso <mariakcaruso@gmail.com>
2018-09-25 04:02:35 +0000

    initial commit with basic code on screens

---
+++ src/components/screens/main.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react'                                                                                  
+//import './App.css'
+/*import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'*/
+
+class Main extends Component {
+    render() {
+	return(
+		<div>main page!</div>
+	)
+    }
+}
+
+export default Main
