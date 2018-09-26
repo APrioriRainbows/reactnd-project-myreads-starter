a903e7c139a9e26480f40f66066d21f5e19c5ca6
Maria Caruso <mariakcaruso@gmail.com>
2018-09-26 01:03:51 +0000

    turn shelves into an obj and set shelfname props equal to book states

--- src/components/BookShelf.js
+++ src/components/BookShelf.js
@@ -2,16 +2,21 @@ import React, {Component} from 'react'
 import Book from './Book.js'
 
 export default class Shelf extends Component {
-   shelves = ["Currently Reading", "Want to Read", "Read"]
-   render(){
+   shelves = {
+       currentlyReading: "Currently Reading", 
+       wantToRead: "Want to Read", 
+       read: "Read"
+}
+//   filteredBooks = bookArray.filter(book => book.state.shelf === this.props.shelfName)
+    render(){
        return(
            <div>
-           {this.shelves.map((shelf, index) => 
-	                <div className="bookshelf">
+           {Object.values(this.shelves).map((shelf, index) => 
+	                <div className="bookshelf" shelfName={Object.keys(this.shelves)[index]}>
                           <h2 className="bookshelf-title">{shelf}</h2>
                           <div className="bookshelf-books">                                                                                        
                             <ol className="books-grid">
-                              <Book/>
+                              <Book/>//write a function to reduce incoming array of book components to ones with the correct state
                             </ol>                                                                                                                   
                           </div>
 	                </div> 	      
