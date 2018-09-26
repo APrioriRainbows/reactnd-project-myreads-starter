283aee0bdfa20c121e2f6fb532664eda6ab2fb61
Maria Caruso <mariakcaruso@gmail.com>
2018-09-26 00:18:04 +0000

    dynamically create shelves

--- src/components/BookShelf.js
+++ src/components/BookShelf.js
@@ -1,19 +1,23 @@
 import React, {Component} from 'react'
 import Book from './Book.js'
+
 export default class Shelf extends Component {
-    shelves = ["Currently Reading", "Want to Read", "Read"]
-    render(){
-	return(
-            <div>
-	      <div className="bookshelf">
-                <h2 className="bookshelf-title">{this.props.shelves}</h2>                                                                             
-                <div className="bookshelf-books">                                                                                                   
-                  <ol className="books-grid">
-                    <Book/>
-                  </ol>                                                                                                                             
-                </div>
-	      </div> 	      
-            </div>
+   shelves = ["Currently Reading", "Want to Read", "Read"]
+   render(){
+       return(
+           <div>
+           {this.shelves.map((shelf, index) => 
+	                <div className="bookshelf">
+                          <h2 className="bookshelf-title">{shelf}</h2>
+                          <div className="bookshelf-books">                                                                                        
+                            <ol className="books-grid">
+                              <Book/>
+                            </ol>                                                                                                                   
+                          </div>
+	                </div> 	      
+                       )
+           }
+           </div>
 	)
     }
 }
