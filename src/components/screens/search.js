961d65c6f33568c81c6e9ac87d30e350444502bf
Maria Caruso <mariakcaruso@gmail.com>
2018-09-27 02:50:29 +0000

    onefiercelinter

--- src/components/screens/search.js
+++ src/components/screens/search.js
@@ -3,19 +3,19 @@ import * as BooksAPI from '../../BooksAPI'
 import Search from '../Search'
 //import './App.css'
 import {
-  BrowserRouter as Router,
-  Route,
-  Link
+    BrowserRouter as Router,
+    Route,
+    Link
 } from 'react-router-dom'
 
 export default class SearchPage extends Component {
     render() {
-	return(
-          <div className="search-books">                                                                                                              
-            <div className="search-books-bar">                                                                                                        
+        return(
+            <div className="search-books">
+                <div className="search-books-bar">
 	      <Link className="close-search" to="/">Close</Link>
-              <div className="search-books-input-wrapper">                                                                                            
-                <Search/>
+                    <div className="search-books-input-wrapper">
+                        <Search/>
 	      </div>
 	    </div>
 	    <div className="search-books-results">
@@ -23,7 +23,7 @@ export default class SearchPage extends Component {
 	      </ol>
 	    </div>
 	  </div>
-	)
+        )
     }
 }
 
