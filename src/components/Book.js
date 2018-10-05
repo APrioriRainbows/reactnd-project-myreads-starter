12aebee9f4778a4dc92306b30e35dcc69a7a2e2d
Maria Caruso <mariakcaruso@gmail.com>
2018-10-05 03:15:32 +0000

    fix book cover default

--- src/components/Book.js
+++ src/components/Book.js
@@ -12,9 +12,9 @@ export default class Book extends Component {
 	const book = this.props.bookinfo
 	const authorsString = (book.authors || []).join(", ")
 	let book_image = book.imageLinks && book.imageLinks.thumbnail
-	book_image = book_image || "http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-yl"
+	book_image = book_image || "https://books.google.com/googlebooks/images/no_cover_thumb.gif"
 	book_image = `url(${book_image})`
-	const style = {width: 128, height: 193, backgroundImage: book_image}
+	const style = {width: 128, height: 193, backgroundSize: 'cover', backgroundImage: book_image}
 	return(
             <li>
               <div className="book">
